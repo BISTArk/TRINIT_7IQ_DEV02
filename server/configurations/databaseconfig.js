5ed7e63cb9d9dbe82d802ec01cac80e0e1c7a09e
Mansi Singh <psingh7274@gmail.com>
2022-01-29 15:03:03 +0530

    db connection bug removed

--- server/configurations/databaseconfig.js
+++ server/configurations/databaseconfig.js
@@ -1,19 +1,16 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-const connectDatabase = async () =>{
-    
-    try {
-        const dbconnection = await mongoose.connect(process.env.MONGO_URI,{
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        })
-        console.log(`MongoDB is connected:${dbconnection.connection.host}`)
-    } catch (error) {
-        console.log(`Error: ${error.message}`)
-        process.exit(1)
-    }
-    
-}
+const connectDatabase = async () => {
+  try {
+    const dbconnection = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB is connected:${dbconnection.connection.host}`);
+  } catch (error) {
+    console.log(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
 
-export default connectDatabase
+export default connectDatabase;
