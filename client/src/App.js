0a1ccafbb69ed3c7e015eec20c3cf1f5f4e2f01e
BISTArk <akshaybistagond@gmail.com>
2022-01-29 18:01:05 +0530

    Landing start

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,21 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Register from "./components/Register.js";
+import Landing from "./pages/Landing.js";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      
-    </div>
-=======
     <Router>
       {/* <Header /> */}
       <main>
         <Routes>
           <Route path='/signup' element={<Register/>} />
+          <Route path='/' element={<Landing/>} />
         </Routes>
       </main>
       {/* <Footer/> */}
     </Router>
 
->>>>>>> fc442d3cdf2e474b947ae53aebd9ddeba424db4d
   );
 }
 
