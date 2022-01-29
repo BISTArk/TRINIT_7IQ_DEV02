13cae11b41888cc39ecbe79aee672698b9d82909
abhishekchavannn <chavanabhishek527@gmail.com>
2022-01-30 02:55:05 +0530

    Auth Frontend done

--- client/src/pages/LoginOrg/Logorg.jsx
+++ client/src/pages/LoginOrg/Logorg.jsx
@@ -1,9 +1,100 @@
-import "./logorg.css"
+import { useState } from "react";
+import "./logorg.css";
 
-function Logorg() {
-  return <div>
-      
-  </div>;
+import { NavLink as Link } from "react-router-dom";
+
+function LogOrg() {
+  const [name, setname] = useState("");
+  const [password, setpassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const re = /^\S+@\S+\.\S+$/;
+    if (name.length < 2) alert("Name should be a minimum of 2 characters long");
+    else if (name.length > 50)
+      alert("Name should be a maximum of 50 characters long");
+    else if (password.length < 6)
+      alert("Password should be a minimum of 6 characters long");
+    else {
+      const data = {
+        Name: name,
+        password: password,
+      };
+      const options = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(data),
+      };
+
+      const response = await fetch(
+        process.env.REACT_APP_SERVER_BASE_URL + "/api/auth/signupOrganizer",
+        options
+      );
+      console.log(response);
+      window.location.href = "/login";
+    }
+  };
+
+  return (
+    <div className="regpage">
+      <div className="sidepic"></div>
+      <div className="mainform">
+        <div className="headerReg">
+          <h1 className="regorgHead">Admin Login</h1>
+        </div>
+        <form className="reg-form">
+          <div className="form-field-reg">
+            <label
+              htmlFor="name"
+              className="input-text"
+              placeholder="Enter organization name"
+            >
+              Organization Name
+            </label>
+
+            <input
+              type="text"
+              name="orgname"
+              placeholder="Enter Organization Name"
+              className="RegInput"
+              value={name}
+              onChange={(e) => {
+                setname(e.target.value);
+              }}
+            />
+          </div>
+          <div className="form-field-reg">
+            <label htmlFor="password" className="input-text ">
+              Password
+            </label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter username"
+              className="RegInput"
+              value={password}
+              onChange={(e) => {
+                setpassword(e.target.value);
+              }}
+            />
+          </div>
+          <span className="already">Register your organization</span>
+          <div className="buttonsReg">
+            <button className=" createorg">Log In</button>
+
+            <Link to="/regorg">
+              <button className="loginorg">Create</button>
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Logorg;
+export default LogOrg;
