587dc37c8d243b519c62f1314ce99ab4b01dbd8a
Mansi Singh <psingh7274@gmail.com>
2022-01-29 15:20:08 +0530

    added import statement in server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,24 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cors from 'cors'
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDatabase from "./configurations/databaseconfig.js";
 
-dotenv.config()
+dotenv.config();
 
-await connectDatabase()
+await connectDatabase();
 
-const app = express()
+const app = express();
 
-app.use(express.json(({limit: '50mb'})))
+app.use(express.json({ limit: "50mb" }));
 
-app.use(cors())
+app.use(cors());
 
 // routes
 
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
     `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
-)
+  )
 );
